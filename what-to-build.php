2e0ab24066fd9ed01e11f3ca99f8a9e4e06135cf
Peter Ton <peter.ton@coolblue.nl>
2023-08-06 12:55:26 +0200

    Bootstrap script to determine what to build

---
+++ what-to-build.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+// Build map of weakness / strengths
+// Load Units data
+// Load Wave 1 data
+// Match units data to wave 1 data
+// Check wave unit armorType and attackType
+// Compare wave units with map
+// Find buildable units matching map
